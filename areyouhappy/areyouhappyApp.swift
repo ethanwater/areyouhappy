ebefd6eb2651fb89cfe33846f34ea7f9dbbe14ba
ethanwater <watermonic@gmail.com>
2025-10-23 17:23:20 -0400

    Initial Commit

---
+++ areyouhappy/areyouhappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  areyouhappyApp.swift
+//  areyouhappy
+//
+//  Created by Ethan Lopez on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct areyouhappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ areyouhappyTests/areyouhappyTests.swift
@@ -0,0 +1,17 @@
+//
+//  areyouhappyTests.swift
+//  areyouhappyTests
+//
+//  Created by Ethan Lopez on 10/23/25.
+//
+
+import Testing
+@testable import areyouhappy
+
+struct areyouhappyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
